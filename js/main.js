48d2cf84dbe2a0de542244e1c8f0854b440ac304
Carlos Alfaro <carlosedu007007@gmail.com>
2021-02-24 22:31:34 -0600

    Update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-/*  Show/Hidden popup login */
-function show_popup_login(){
-	let popup = document.querySelector(".popup-login");
-	if(popup.classList.contains('active')){
-		popup.classList.remove('active');
+/*  Show/Hidden menu mobile */
+function show_menu_mobile(){
+	let header_navbar = document.querySelector(".header-navbar");
+	let body = document.getElementById('main-body');
+	if(header_navbar.classList.contains('active')){
+		header_navbar.classList.remove('active');
+		body.classList.remove('blocked');
 	}else{
-		popup.classList.add('active');
+		header_navbar.classList.add('active');
+		body.classList.add('blocked');
 	}
 }
